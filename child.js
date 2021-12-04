7e1e33b7b94764134e9b19828b8452da7543145f
Kensuke Morimoto <morimotokensuke0104@gmail.com>
2021-12-04 15:30:43 +0900

    写真を押したら文が出る

---
+++ child.js
@@ -0,0 +1,8 @@
+function clicked(){
+    const pic3 = document.getElementById("explain1");
+    pic3.style.visibility = "visible";
+}
+function clickAgain(){
+    const pic4 = document.getElementById("explain2");
+    pic4.style.visibility = "visible";
+}
