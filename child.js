e37fd208e043234c45e5fc82fe35bd46ce14a948
Kensuke Morimoto <morimotokensuke0104@gmail.com>
2021-12-09 22:38:14 +0900

    完成

--- child.js
+++ child.js
@@ -1,3 +1,15 @@
+const manbou = document.getElementById("manbou")
+const dolphin = document.getElementById("dolphin")
+
+manbou.addEventListener('click',() => {
+    const manbouContent = document.getElementById("manbouContent")
+    manbouContent.classList.add('visible')
+},false)
+dolphin.addEventListener('click',() => {
+    const dolphinContent = document.getElementById("dolphinContent")
+    dolphinContent.classList.add('visible')
+},false)
+
 function clicked(){
     const pic3 = document.getElementById("explain1");
     pic3.style.visibility = "visible";
